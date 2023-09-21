1ead78b686027b66050ffc23aae439e81a535a55
Pavan Kumar <pavan_2004it@icloud.com>
2023-09-21 09:50:44 +0530

    added stateless config to forward traffic to sfe

--- main.tf
+++ main.tf
@@ -16,8 +16,8 @@ resource "aws_networkfirewall_firewall_policy" "rp-policy" {
   name = var.rule_policy_name
 
   firewall_policy {
-    stateless_default_actions = ["aws:pass"]
-    stateless_fragment_default_actions = ["aws:drop"]
+    stateless_default_actions = ["aws:forward_to_sfe"]
+    stateless_fragment_default_actions = ["aws:forward_to_sfe"]
     stateful_rule_group_reference {
       resource_arn = aws_networkfirewall_rule_group.rp_rule_group.arn
     }
